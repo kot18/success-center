773ff27f6000c4c58ddc155f29c017060e084624
Kot <kis-cot.kot@yandex.ru>
2023-12-09 21:18:34 +0300

    нихуя не понимаю

--- src/components/userProfileComponent.jsx
+++ src/components/userProfileComponent.jsx
@@ -1,20 +1,30 @@
 import React from 'react';
-import classes from "../css/news.module.css"
+import classes from "../css/userProfile.module.css"
+import avatar from "../images/people/avatar_scaled.jpeg"
 
-const NewsComponent = () => {
+const UserComponent = () => {
     return (
         <section className={classes.sectionContainer}>
             <div className={classes.imgContainer}>
-                <img></img>
+                {/*<button className={classes.userInfoItemButton}>Изменить фото</button>*/}
+                {/* eslint-disable-next-line jsx-a11y/img-redundant-alt */}
+                <img className={classes.photoProfile} src={avatar} alt="user photo"></img>
             </div>
             <div className={classes.userInfoContainer}>
-                <div className={classes.loginContainer}>
-                    <p>Почта: {'user-login@yandex.ru'}</p>
-                    <button>Сменить почту</button>
+                <div className={classes.userInfoItem}>
+                    <p>Дата рождения: {"03.02.2003"}</p>
+                    <button className={classes.userInfoItemButton}>Изменить</button>
                 </div>
-                <div className={classes.passwordContainer}>
+                <div className={classes.userInfoItem}>
+                    <p>Гендер: {"Мужской"}</p>
+                </div>
+                <div className={classes.userInfoItem}>
+                    <p>Почта: {'user-login@yandex.ru'}</p>
+                    <button className={classes.userInfoItemButton}>Сменить почту</button>
+                </div>
+                <div className={classes.userInfoItem}>
                     <p>Пароль: {'********'}</p>
-                    <button>Изменить пароль</button>
+                    <button className={classes.userInfoItemButton}>Изменить пароль</button>
                 </div>
             </div>
             <div className={classes.lastCommentsContainer}>
@@ -24,4 +34,4 @@ const NewsComponent = () => {
     );
 };
 
-export default NewsComponent;
+export default UserComponent;
--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import Profile from "../components/profileComponent";
 import FooterComponent from "../components/footerComponent";
+import UserComponent from "../components/userProfileComponent";
+import NavigationComponent from "../components/navigationComponent";
 
 const NewsAndArticles = () => {
     return (
         <>
-            <Profile></Profile>
+            <NavigationComponent></NavigationComponent>
+            <UserComponent></UserComponent>
             <FooterComponent></FooterComponent>
         </>
     );
